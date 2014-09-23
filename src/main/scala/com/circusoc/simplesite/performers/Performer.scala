b6e8ef996783d83bc88da7dce1a18e1d40c414f7
Richard Weiss <richardweiss@richardweiss.org>
2014-09-23 23:29:34 +1000

    Basic performer stuff

--- src/main/scala/com/circusoc/simplesite/performers/Performer.scala
+++ src/main/scala/com/circusoc/simplesite/performers/Performer.scala
@@ -2,6 +2,7 @@ package com.circusoc.simplesite.performers
 
 import spray.json._
 import java.net.URL
+import com.circusoc.simplesite.WithConfig
 
 
 /**
@@ -17,40 +18,43 @@ case class Performer(id: Long,
                 otherPictures: Set[PictureURL]) {}
 
 
-object PerformerJSONProtocol extends DefaultJsonProtocol {
-  implicit object PerformerJsonFormat extends RootJsonFormat[Performer] {
-    import Skill.SkillJsonFormat._
-    import PictureURL.PictureURLJsonFormat._
+/**
+ * The JSON formatter for a performer.
+ * This needs to be instantiated as a class because it depends on the picture formatter.
+ * @param config
+ */
+class PerformerJsonFormat(implicit config: WithConfig) extends RootJsonFormat[Performer] with DefaultJsonProtocol {
+  import Skill.SkillJsonFormat._
 
-    def write(performer: Performer) =
-      JsObject(
-        "id" -> JsNumber(performer.id),
-        "username" -> JsString(performer.name),
-        "skills" -> performer.skills.toJson,
-        "profile_picture" -> performer.profilePicture.toJson,
-        "other_pictures" -> performer.otherPictures.toJson
-      )
+  implicit val pictureJsonFormatter = new PictureURLJsonFormatter()
+  def write(performer: Performer) =
+    JsObject(
+      "id" -> JsNumber(performer.id),
+      "username" -> JsString(performer.name),
+      "skills" -> performer.skills.toJson,
+      "profile_picture" -> performer.profilePicture.toJson,
+      "other_pictures" -> performer.otherPictures.toJson
+    )
 
-    def read(value: JsValue): Performer = value match {
-      case o: JsObject =>
-        val fields = o.fields
-        val performerFields = for {
-          id <- fields.get("id")
-          username <- fields.get("name")
-          skills <- fields.get("skills")
-          profilePicture <- fields.get("profile_picture")
-          otherPictures <- fields.get("other_pictures")
-        } yield (id, username, skills, profilePicture, otherPictures)
-        performerFields match {
-          case Some((JsNumber(id), JsString(name), JsArray(_skills), _profilePic, JsArray(_otherPics))) =>
-            val skills = _skills.map(_.convertTo[Skill]).toSet
-            val performerPic = _profilePic.convertTo[PictureURL]
-            val otherPics = _otherPics.map(_.convertTo[PictureURL]).toSet
-            Performer(id.toLong, name, skills, performerPic, otherPics)
-          case _ => deserializationError("Perfomer expected")
-        }
-      case _ => deserializationError("Performer expected")
-    }
+  def read(value: JsValue): Performer = value match {
+    case o: JsObject =>
+      val fields = o.fields
+      val performerFields = for {
+        id <- fields.get("id")
+        username <- fields.get("name")
+        skills <- fields.get("skills")
+        profilePicture <- fields.get("profile_picture")
+        otherPictures <- fields.get("other_pictures")
+      } yield (id, username, skills, profilePicture, otherPictures)
+      performerFields match {
+        case Some((JsNumber(id), JsString(name), JsArray(_skills), _profilePic, JsArray(_otherPics))) =>
+          val skills = _skills.map(_.convertTo[Skill]).toSet
+          val performerPic = _profilePic.convertTo[PictureURL]
+          val otherPics = _otherPics.map(_.convertTo[PictureURL]).toSet
+          Performer(id.toLong, name, skills, performerPic, otherPics)
+        case _ => deserializationError("Perfomer expected")
+      }
+    case _ => deserializationError("Performer expected")
   }
 }
 
@@ -64,14 +68,28 @@ object Skill extends DefaultJsonProtocol {
     }
   }
 }
-case class PictureURL(url: URL) extends AnyVal {}
 
-object PictureURL extends DefaultJsonProtocol {
-  implicit object PictureURLJsonFormat extends RootJsonFormat[PictureURL] {
-    def write(link: PictureURL) = JsString(link.url.toExternalForm)
-    def read(value: JsValue) = value match {
-      case JsString(v) => PictureURL(new URL(v))
-      case _ => deserializationError("URL expected")
-    }
+case class PictureURL(id: Long, url: URL)
+
+object PictureURL extends  {
+  def fromURL(url: URL)(implicit config: WithConfig) = ???
+}
+
+/**
+ * The picture formatter. This is instantiated as a class so you can
+ * get the config, which happens in the usual implicit config way.
+ * Just do:
+ *
+ *  implicit val config = something
+ *  ...
+ *  implicit val pictureURLJsonFormatter = new PictureURLJsonFormatter()
+ *
+ * @param config the config we're after.
+ */
+class PictureURLJsonFormatter(implicit config: WithConfig) extends RootJsonFormat[PictureURL] with DefaultJsonProtocol {
+  def write(link: PictureURL) = JsString(link.url.toExternalForm)
+  def read(value: JsValue) = value match {
+    case JsString(v) => PictureURL.fromURL(new URL(v))
+    case _ => deserializationError("URL expected")
   }
 }
